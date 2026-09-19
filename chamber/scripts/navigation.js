b428ca5371174d40d39457b2321b20ccb274d939
lucasfeliciana <lfeliciana@byupathway.edu>
2026-09-19 13:58:48 -0500

    changing the logo

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,7 +1,6 @@
-const menuButton = document.querySelector("#menu");
+const menuButton = document.querySelector("#menu-button");
 const navigation = document.querySelector("#navigation");
 
 menuButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
-    menuButton.classList.toggle("open");
 });
